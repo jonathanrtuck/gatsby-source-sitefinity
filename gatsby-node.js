cb82c5a3dd35975c89ab88edd3debbfceca55a3a
jonathan tucker <github@jrtucker.com>
2018-11-25 09:50:48 -0600

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,153 @@
+const axios = require('axios');
+const crypto = require('crypto');
+
+/**
+ * @constant
+ * @function
+ * @param {string} str
+ */
+const error = (str) => {
+  console.error('\x1b[31m%s\x1b[0m', '\nerror', str);
+};
+
+/**
+ * @constant
+ * @function
+ * @param {string} str
+ */
+const success = (str) => {
+  console.log('\x1b[32m%s\x1b[0m', '\nsuccess', str);
+};
+
+/**
+ * @function
+ * @param {object} obj
+ * @param {object} obj.actions
+ * @param {function} obj.createNodeId
+ * @param {object} configOptions
+ * @param {string} configOptions.url
+ * @returns {Promise}
+ * @see https://www.gatsbyjs.org/docs/source-plugin-tutorial/
+ * @see https://www.gatsbyjs.org/docs/node-apis/#sourceNodes
+ */
+exports.sourceNodes = ({actions, createNodeId}, {url}) => {
+  /**
+   * validate configuration options.
+   */
+  if (typeof url !== 'string' || url === '') {
+    return error('you must provide a sitefinity api url in the gatsby-source-sitefinity plugin in gatsby-config.js');
+  }
+
+  /**
+   * @constant
+   * @type {function}
+   */
+  const {createNode} = actions;
+  /**
+   * remove trailing slash.
+   * @constant
+   * @type {string}
+   */
+  const siteUrl = url.replace(/\/$/, '');
+
+  /**
+   * get list of content types.
+   * @type {Promise}
+   */
+  return axios({
+    url: siteUrl,
+  })
+    .then(
+      /**
+       * create and fire request for each content type.
+       * @function
+       * @param {object} response
+       * @returns {Promise}
+       */
+      (response) =>
+        axios.all(
+          response.data.value
+            .map(
+              /**
+               * create axios configuration. save 'name' property value for use as content type identifier.
+               * @function
+               * @param {object} obj
+               * @returns {object}
+               */
+              (obj) => ({
+                _type: obj.name,
+                url: `${siteUrl}/${obj.url}?$expand=*`,
+              })
+            )
+            .map(axios)
+        )
+    )
+    .then(
+      /**
+       * parse responses and create gatsby nodes for all content items.
+       * @function
+       * @param {object[]} responses
+       */
+      (responses) =>
+        responses
+          .reduce(
+            /**
+             * create array of all content items.
+             * @function
+             * @param {object[]} arr
+             * @param {object} response
+             * @returns {object[]}
+             */
+            (arr, response) =>
+              arr.concat(
+                response.data.value.map(
+                  /**
+                   * add getsby node properties.
+                   * @function
+                   * @param {object} obj
+                   * @returns {object}
+                   */
+                  (obj) => ({
+                    ...obj,
+                    children: [],
+                    id: createNodeId(obj.Id),
+                    internal: {
+                      content: JSON.stringify(obj),
+                      contentDigest: crypto
+                        .createHash('md5')
+                        .update(JSON.stringify(obj))
+                        .digest('hex'),
+                      // mediaType: obj.MimeType,
+                      type: `Sitefinity${response.config._type}`, // prepend to avoid naming collisions with other source plugins
+                    },
+                    parent: null,
+                  })
+                )
+              ),
+            []
+          )
+          .forEach(
+            /**
+             * create gatsby node for each content item.
+             * @function
+             * @param {object} obj
+             */
+            (obj) => createNode(obj)
+          )
+    )
+    .then(
+      /**
+       * @function
+       */
+      () => success('create sitefinity content nodes')
+    )
+    .catch(
+      /**
+       * @function
+       */
+      () =>
+        error(
+          'cannot get content from sitefinity. have you provided the correct url in the gatsby-source-sitefinity plugin in gatsby-config.js?'
+        )
+    );
+};
